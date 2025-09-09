46a43826a3eaf17fe33275672a74195b099a47f9
thomasalfa12 <edison.thomas52@gmail.com>
2025-09-09 18:19:48 +0700

    fix: correct page props typing for build

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,17 +1,17 @@
-// src/app/menu/[category]/page.tsx
 import { menuData } from "@/data/mock";
-import { FullMenu } from "@/components/sections/menu/FullMenu"; // <-- Impor komponen baru
+import { FullMenu } from "@/components/sections/menu/FullMenu";
 
-export default function FullMenuPage({
-  params,
-}: {
-  params: { category: string };
-}) {
-  // Tugas halaman ini sekarang hanya 2:
-  // 1. Mengambil SEMUA data menu.
+// 1. Definisikan tipe untuk props di sini menggunakan interface
+interface FullMenuPageProps {
+  params: {
+    category: string;
+  };
+}
+
+// 2. Gunakan interface yang sudah dibuat sebagai tipe untuk props
+export default function FullMenuPage({ params }: FullMenuPageProps) {
   const allMenuItems = menuData;
 
-  // 2. Menyerahkan data dan kategori awal dari URL ke Client Component.
   return (
     <div className="container mx-auto px-6 py-24 pt-36 min-h-screen">
       <FullMenu allMenuItems={allMenuItems} initialCategory={params.category} />
